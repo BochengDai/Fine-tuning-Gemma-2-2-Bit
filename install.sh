0e8ba0afdc50e0334c9e5f7e2bd34bce3eb70ebe
BochengDai <144160576+BochengDai@users.noreply.github.com>
2025-03-26 23:18:49 -0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+pip install transformers datasets
+pip install scikit-learn
+pip install sentencepiece
+pip install matplotlib seaborn
+pip install peft
+pip install modelscope
+pip install bitsandbytes
+pip install -U "transformers==4.42.3" accelerate
